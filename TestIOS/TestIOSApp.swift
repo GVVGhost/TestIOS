2c84af58ca95326c05d57857e4054cea48c73bb5
Vadim <gvv.ghost@gmail.com>
2024-08-13 14:53:15 +0300

    Initial commit

---
+++ TestIOS/TestIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestIOSApp.swift
+//  TestIOS
+//
+//  Created by Vadym Horeniuck on 13.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
